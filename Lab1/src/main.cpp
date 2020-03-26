cebeec634e0f744f724b341bb245a161d7421e8b
Boyy-Next-Door <945454050@qq.com>
2020-03-27 00:08:49 +0800

    修改了main.cc中关于信号量、任务池的设计 基本明确了实现思路

--- Lab1/src/main.cpp
+++ Lab1/src/main.cpp
@@ -22,16 +22,56 @@ void thread_function filenameReader() {
     std::cout << "thread function excuting" << std::endl;//添加读入 
 }
 
-int main(){
-sem_t problemEmpty;    //信号量 
-sem_t problemFull;
-sem_t problemMutex;
+/*任务结构体 直接在dancinglinck里文件流输出结果 不用考虑返回结果数组了*/
+struct job{
+	int board[81]; //题目
+};
+
+/*任务池大小*/
+#define POOL_SIZE 1000	//这个需要整体实现完之后进行调参
+/*任务单元大小*/ 
+#define JOB_UNIT_SIZE 100	//输入线程or解题线程以这个大小为单位一次性读取or消费这么多个题目
+/*信号量数值上限*/
+#define SEM_MAXIMUM 10	//这个值为任务池大小/任务单元大小
+
+struct job[POOL_SIZE];	//任务池
+
+
+/*信号量	*/	
+//！！！	注意！！！  任务池中当前任务数量n = poolFull.currNum * JOB_UNIT_SIZE     
+sem_t pollEmpty;//任务池空 
+//--> 读取线程 当pollEmpty大于0时直接返回（空位足够） 等于0则睡眠（塞不下了）
+sem_t pollFull;	//任务池满
+//--> 解题线程 当poolFull大于0时直接返回（任务足够） 等于0则睡眠（没任务）
+
+//任务池锁
+pthread_mutex_t jobPoolMutex=PTHREAD_MUTEX_INITIALIZER;
+
+//分段式读入可能需要的位置标记
+//int flag=0;
+
+
+/*
+感觉暂时用不到这些
+sem_t problemMutex;		
 sem_t filenameEmpty;
 sem_t filenameFull;
-sem_t filenameMutex;
-//     使用时参考这个形式 sem_wait(&filenameEmpty); sem_wait(&filenameBufferMutex);
+sem_t filenameMutex; 
+*/
+
+int sem_init __P ((sem_t *__sem, int __pshared, unsigned int __value));
+
+int main(){
+		//初始化两个信号量
+		sem_init(&poolEmpty,0,SEM_MAXIMUM);
+		sem_init(&poolFull,0,0);
+//     使用时参考这个形式 
+//		sem_wait(&pollEmpty); 
+//		sem_wait(&pollFull);  
           
-//        sem_post(&filenameBufferMutex);   sem_post(&filenameBufferFull);     wait和post都是原子操作  可以直接用 
+//      sem_post(&pollEmpty);
+//		sem_post(&pollFull);     
+//		wait和post都是原子操作  可以直接用 
 	
     string   Stime = getTime();   //起始时间   
     cout << Stime << endl;
